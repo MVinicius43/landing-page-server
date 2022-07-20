7ee49f1cf7c6f28547f18d9624c0ff5c3c6bfb88
Marcos Silva (PSV) <marcos.silva@psvsistemas.com.br>
2022-07-20 14:27:35 -0300

    feat: nodemailer configuration

--- src/use-cases/create-user-use-case.ts
+++ src/use-cases/create-user-use-case.ts
@@ -1,9 +1,11 @@
+import { MailAdapter } from '../adapters/mail-adapter';
 import { UsersRepository } from '../repositories/users-repository'
 import { ICreateUserRequestDTO } from './create-user-DTO';
 
 export class CreateUserUseCase {
   constructor (
-    private userRepository: UsersRepository
+    private userRepository: UsersRepository,
+    private mailAdapter: MailAdapter
   ) {}
 
   async execute(request: ICreateUserRequestDTO) {
@@ -30,6 +32,20 @@ export class CreateUserUseCase {
       email,
       contact,
       company,
+    })
+
+    await this.mailAdapter.sendMail({
+      subject: 'Novo usuário cadastrado em sua Landing Page',
+      body: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+        `<p>O usuário ${name} se inscreveu em sua Landing Page.</p>`,
+        `<p>Segue abaixo os dados:</p>`,
+        `<p>Nome: ${name}</p>`,
+        `<p>Email: ${email}</p>`,
+        `<p>Contato: ${contact}</p>`,
+        `<p>Empresa em que atua: ${company}</p>`,
+        `</div>`
+      ].join('\n')
     })
   }
 }
